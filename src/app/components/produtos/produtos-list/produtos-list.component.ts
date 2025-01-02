0f991b5a03a8e25e094b4871c4a9959ffd56446e
Breno Chiori <brenochiori871@gmail.com>
2025-01-02 10:43:35 -0300

    feat: create list for produtos-list compoment

--- src/app/components/produtos/produtos-list/produtos-list.component.ts
+++ src/app/components/produtos/produtos-list/produtos-list.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatTableDataSource } from '@angular/material/table';
+import { Produtos } from 'src/app/models/produtos';
+import { FornecedorService } from 'src/app/services/fornecedor.service';
+import { ProdtutosService } from 'src/app/services/prodtutos.service';
 
 @Component({
   selector: 'app-produtos-list',
@@ -7,9 +12,41 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProdutosListComponent implements OnInit {
 
-  constructor() { }
+  ELEMENT_DATA: Produtos[] = []
+  FILTERED_DATA: Produtos[] = []
+
+  displayedColumns: string[] = ['id', 'marca', 'nomeProduto', 'fornecedor', 'acoes']
+  dataSource = new MatTableDataSource<Produtos>(this.ELEMENT_DATA)
+
+  @ViewChild(MatPaginator) paginator: MatPaginator
+
+  constructor(
+    private produtoService: ProdtutosService, 
+    private fornecedorService: FornecedorService) { }
 
   ngOnInit(): void {
+    this.findAll()
   }
 
+  findAll() {
+    this.produtoService.findAll().subscribe(resposta => {
+      this.ELEMENT_DATA = resposta
+      this.findFornecedorName()
+      this.dataSource = new MatTableDataSource<Produtos>(resposta)
+      this.dataSource.paginator = this.paginator
+    })
+  }
+
+  findFornecedorName() {
+    this.ELEMENT_DATA.forEach(produto => {
+      this.fornecedorService.findById(produto.fornecedor).subscribe(resposta => {
+        produto.fornecedor = resposta.nome
+      })
+    })
+  }
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+  }
 }
