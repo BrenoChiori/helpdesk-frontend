69351bad26e2894ad79b33961a49a0d4fa268d11
Breno Chiori <brenochiori871@gmail.com>
2025-01-08 09:58:09 -0300

    fix

--- src/app/components/chamado/chamado-create/chamado-create.component.ts
+++ src/app/components/chamado/chamado-create/chamado-create.component.ts
@@ -33,7 +33,7 @@ export class ChamadoCreateComponent implements OnInit {
   prioridade: FormControl = new FormControl(null, [Validators.required])
   status:     FormControl = new FormControl(null, [Validators.required])
   titulo:     FormControl = new FormControl(null, [Validators.required])
-  observacao:FormControl = new FormControl(null, [Validators.required])
+  observacao: FormControl = new FormControl(null, [Validators.required])
   tecnico:    FormControl = new FormControl(null, [Validators.required])
   cliente:    FormControl = new FormControl(null, [Validators.required])
 
